f36ed2630aaaf9e3e254a6c4155b47ad1d1e9031
Harut <badalyanharutyun@gmail.com>
2024-08-18 08:17:59 +0400

    in getAll added limit,columns if exist it select only column and checkConfig validaton if config not exist throw error

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const knex = require('knex');
 class BaseModel {
-    static config
-  static async getAll() {
-    const query = knex(this.config)
-    return await query.select('*').from(this.table)
+  static config;
+  static columns;
+  static select;
+  static checkConfig() {
+    if (!this.config) {
+      throw new Error('messyorm need config (it is same as knex config)');
+    }
+  }
+  static async getAll(options) {
+    this.checkConfig();
+    let query = knex(this.config);
+    let queryBuilder = query(this.table);
+    if (!this.columns) {
+      return await options?.limit? queryBuilder.select('*').limit(options.limit):queryBuilder.select('*');
+    }
+    if (!this.select) {
+      for (const item of this.columns) {
+        queryBuilder.select(item);
+      }
+      return   await options?.limit? queryBuilder.limit(options.limit):queryBuilder;
+    }
   }
 }
-module.exports=BaseModel
+module.exports = BaseModel;
